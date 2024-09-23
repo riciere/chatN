dfc449eb74ec730a8f8ddb8ccb90aa01d1a2308d
Riciere Tasca <riciere@gmail.com>
2024-09-22 21:48:06 -0300

    correcao erros
    tentativa nova

--- app/mailers/conversation_reply_mailer_helper.rb
+++ app/mailers/conversation_reply_mailer_helper.rb
@@ -1,105 +1,203 @@
-module ConversationReplyMailerHelper
-  def prepare_mail(cc_bcc_enabled)
-    @options = {
-      to: to_emails,
-      from: email_from,
-      reply_to: email_reply_to,
-      subject: mail_subject,
-      message_id: custom_message_id,
-      in_reply_to: in_reply_to_email
-    }
+class ConversationReplyMailer < ApplicationMailer
+  include ConversationReplyMailerHelper
+  default from: ENV.fetch('MAILER_SENDER_EMAIL', 'Chatwoot <accounts@chatwoot.com>')
+  layout :choose_layout
 
-    if cc_bcc_enabled
-      @options[:cc] = cc_bcc_emails[0]
-      @options[:bcc] = cc_bcc_emails[1]
-    end
-    ms_smtp_settings
-    google_smtp_settings
-    set_delivery_method
+  def reply_with_summary(conversation, last_queued_id)
+    return unless smtp_config_set_or_development?
 
-    Rails.logger.info("Email enviado de #{email_from} para #{to_emails} com assunto #{mail_subject}")
+    init_conversation_attributes(conversation)
+    return if conversation_already_viewed?
 
-    mail(@options)
+    recap_messages = @conversation.messages.chat.where('id < ?', last_queued_id).last(10)
+    new_messages = @conversation.messages.chat.where('id >= ?', last_queued_id)
+    @messages = recap_messages + new_messages
+    @messages = @messages.select(&:email_reply_summarizable?)
+    prepare_mail(true)
+  end
+
+  def reply_without_summary(conversation, last_queued_id)
+    return unless smtp_config_set_or_development?
+
+    init_conversation_attributes(conversation)
+    return if conversation_already_viewed?
+
+    @messages = @conversation.messages.chat.where(message_type: [:outgoing, :template]).where('id >= ?', last_queued_id)
+    @messages = @messages.reject { |m| m.template? && !m.input_csat? }
+    return false if @messages.count.zero?
+
+    prepare_mail(false)
+  end
+
+  def email_reply(message)
+    return unless smtp_config_set_or_development?
+
+    init_conversation_attributes(message.conversation)
+    @message = message
+#new code
+    @previous_message = @conversation.messages.chat.where.not(id: message).last
+    reply_mail_object = prepare_mail(true)
+    message.update(source_id: reply_mail_object.message_id)
+  end
+
+  def conversation_transcript(conversation, to_email)
+    return unless smtp_config_set_or_development?
+
+    init_conversation_attributes(conversation)
+
+    @messages = @conversation.messages.chat.select(&:conversation_transcriptable?)
+
+    Rails.logger.info("Email sent from #{from_email_with_name} \
+      to #{to_email} with subject #{@conversation.display_id} \
+      #{I18n.t('conversations.reply.transcript_subject')} ")
+    mail({
+           to: to_email,
+           from: from_email_with_name,
+           subject: "[##{@conversation.display_id}] #{I18n.t('conversations.reply.transcript_subject')}"
+         })
   end
 
   private
 
-  def google_smtp_settings
-    return unless @inbox.email? && @channel.imap_enabled && @inbox.channel.google?
-
-    smtp_settings = base_smtp_settings('smtp.gmail.com')
-
-    @options[:delivery_method] = :smtp
-    @options[:delivery_method_options] = smtp_settings
+  def init_conversation_attributes(conversation)
+    @conversation = conversation
+    @account = @conversation.account
+    @contact = @conversation.contact
+    @agent = @conversation.assignee
+    @inbox = @conversation.inbox
+    @channel = @inbox.channel
   end
 
-  def ms_smtp_settings
-    return unless @inbox.email? && @channel.imap_enabled && @inbox.channel.microsoft?
-
-    smtp_settings = base_smtp_settings('smtp.office365.com')
-
-    @options[:delivery_method] = :smtp
-    @options[:delivery_method_options] = smtp_settings
+  def should_use_conversation_email_address?
+    @inbox.inbox_type == 'Email' || inbound_email_enabled?
   end
 
-  def base_smtp_settings(domain)
-    {
-      address: domain,
-      port: 587,
-      user_name: @channel.imap_login,
-      password: @channel.provider_config['access_token'],
-      domain: domain,
-      tls: false,
-      enable_starttls_auto: true,
-      openssl_verify_mode: 'none',
-      authentication: 'xoauth2'
-    }
+  def conversation_already_viewed?
+    # whether contact already saw the message on widget
+    return unless @conversation.contact_last_seen_at
+    return unless last_outgoing_message&.created_at
+
+    @conversation.contact_last_seen_at > last_outgoing_message&.created_at
   end
 
-  def set_delivery_method
-    return unless @inbox.inbox_type == 'Email' && @channel.smtp_enabled
-
-    smtp_settings = {
-      address: @channel.smtp_address,
-      port: @channel.smtp_port,
-      user_name: @channel.smtp_login,
-      password: @channel.smtp_password,
-      domain: @channel.smtp_domain,
-      tls: @channel.smtp_enable_ssl_tls,
-      enable_starttls_auto: @channel.smtp_enable_starttls_auto,
-      openssl_verify_mode: @channel.smtp_openssl_verify_mode,
-      authentication: @channel.smtp_authentication
-    }
-
-    @options[:delivery_method] = :smtp
-    @options[:delivery_method_options] = smtp_settings
+  def last_outgoing_message
+    @conversation.messages.chat.where.not(message_type: :incoming)&.last
   end
 
-  def email_smtp_enabled
-    @inbox.inbox_type == 'Email' && @channel.smtp_enabled
+  def sender_name(sender_email)
+    if @inbox.friendly?
+      I18n.t('conversations.reply.email.header.friendly_name', sender_name: custom_sender_name, business_name: business_name,
+                                                               from_email: sender_email)
+    else
+      I18n.t('conversations.reply.email.header.professional_name', business_name: business_name, from_email: sender_email)
+    end
   end
 
-  def email_imap_enabled
-    @inbox.inbox_type == 'Email' && @channel.imap_enabled
+  def current_message
+    @message || @conversation.messages.outgoing.last
   end
 
-  def email_oauth_enabled
-    @inbox.inbox_type == 'Email' && (@channel.microsoft? || @channel.google?)
+  def custom_sender_name
+    current_message&.sender&.available_name || @agent&.available_name || 'Notifications'
   end
 
-  def email_from
-    email_oauth_enabled || email_smtp_enabled ? channel_email_with_name : from_email_with_name
+  def business_name
+    @inbox.business_name || @inbox.name
   end
 
-  def email_reply_to
-    email_imap_enabled ? @channel.email : reply_email
+  def from_email
+    should_use_conversation_email_address? ? parse_email(@account.support_email) : parse_email(inbox_from_email_address)
   end
 
-  # Use channel email domain in case of account email domain is not set for custom message_id and in_reply_to
-  def channel_email_domain
-    return @account.inbound_email_domain if @account.inbound_email_domain.present?
+  def mail_subject
+    subject = @conversation.additional_attributes['mail_subject']
+    return "[##{@conversation.display_id}] #{I18n.t('conversations.reply.email_subject')}" if subject.nil?
 
-    email = @inbox.channel.try(:email)
-    email.present? ? email.split('@').last : raise(StandardError, 'Domínio de email do canal não está presente.')
+    chat_count = @conversation.messages.chat.count
+    if chat_count > 1
+      "Re: #{subject}"
+    else
+      subject
+    end
+  end
+
+  def reply_email
+    if should_use_conversation_email_address?
+      sender_name("reply+#{@conversation.uuid}@#{@account.inbound_email_domain}")
+    else
+      @inbox.email_address || @agent&.email
+    end
+  end
+
+  def from_email_with_name
+    sender_name(from_email)
+  end
+
+  def channel_email_with_name
+    sender_name(@channel.email)
+  end
+
+  def parse_email(email_string)
+    Mail::Address.new(email_string).address
+  end
+
+  def inbox_from_email_address
+    return @inbox.email_address if @inbox.email_address
+
+    @account.support_email
+  end
+
+  def custom_message_id
+    last_message = @message || @messages&.last
+
+    "<conversation/#{@conversation.uuid}/messages/#{last_message&.id}@#{channel_email_domain}>"
+  end
+
+  def in_reply_to_email
+    conversation_reply_email_id || "<account/#{@account.id}/conversation/#{@conversation.uuid}@#{channel_email_domain}>"
+  end
+
+  def conversation_reply_email_id
+    content_attributes = @conversation.messages.incoming.last&.content_attributes
+
+    if content_attributes && content_attributes['email'] && content_attributes['email']['message_id']
+      return "<#{content_attributes['email']['message_id']}>"
+    end
+
+    nil
+  end
+
+  def cc_bcc_emails
+    content_attributes = @conversation.messages.outgoing.last&.content_attributes
+
+    return [] unless content_attributes
+    return [] unless content_attributes[:cc_emails] || content_attributes[:bcc_emails]
+
+    [content_attributes[:cc_emails], content_attributes[:bcc_emails]]
+  end
+
+  def to_emails_from_content_attributes
+    content_attributes = @conversation.messages.outgoing.last&.content_attributes
+
+    return [] unless content_attributes
+    return [] unless content_attributes[:to_emails]
+
+    content_attributes[:to_emails]
+  end
+
+  def to_emails
+    # if there is no to_emails from content_attributes, send it to @contact&.email
+    to_emails_from_content_attributes.presence || [@contact&.email]
+  end
+
+  def inbound_email_enabled?
+    @inbound_email_enabled ||= @account.feature_enabled?('inbound_emails') && @account.inbound_email_domain
+                                                                                      .present? && @account.support_email.present?
+  end
+
+  def choose_layout
+    return false if action_name == 'reply_without_summary' || action_name == 'email_reply'
+
+    'mailer/base'
   end
 end
